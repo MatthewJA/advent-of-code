1e7487d41f095f7d255b79d493e74f97d2180a0c
MatthewJA <matthew.alger@gmail.com>
2025-03-16 21:40:36 +1100

    Day 6/2.

---
+++ 2024/day6/go.mod
@@ -0,0 +1,12 @@
+module alger.au/aoc/2024/day6
+
+go 1.23.4
+
+require github.com/schollz/progressbar/v3 v3.18.0
+
+require (
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/term v0.30.0 // indirect
+)
--- 2024/day6/guard_gallivant.go
+++ 2024/day6/guard_gallivant.go
@@ -5,13 +5,20 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"slices"
+
+	"github.com/schollz/progressbar/v3"
 )
 
 const dataPath = "../data/day6.txt"
 
 // parse parses the grid data into a grid of bytes.
 func parse(data []byte) [][]byte {
-	return bytes.Split(data, []byte("\n"))
+	rows := bytes.Split(data, []byte("\n"))
+	for i, row := range rows {
+		rows[i] = bytes.TrimSuffix(row, []byte("\r"))
+	}
+	return rows
 }
 
 // printGrid prints a grid to stdout.
@@ -26,9 +33,10 @@ func printGrid(grid [][]byte) {
 
 type simulator struct {
 	// By caching the guard location we can avoid many lookups.
-	guardY int
-	guardX int
-	grid   [][]byte
+	guardY      int
+	guardX      int
+	grid        [][]byte
+	guardStates [][][]byte
 }
 
 // rotate rotates the guard.
@@ -48,8 +56,8 @@ func rotate(guard byte) (byte, error) {
 }
 
 // step mutates the grid to simulate the guard moving one step.
-// It returns whether the guard is still on the grid.
-func (s *simulator) step() (bool, error) {
+// It returns whether the guard is still on the grid and whether the guard has looped.
+func (s *simulator) step() (bool, bool, error) {
 	dx, dy := 0, 0
 	guard := s.grid[s.guardY][s.guardX]
 	switch guard {
@@ -62,7 +70,7 @@ func (s *simulator) step() (bool, error) {
 	case 'v':
 		dy = 1
 	default:
-		return false, fmt.Errorf("invalid guard: %v", guard)
+		return false, false, fmt.Errorf("invalid guard: %v", guard)
 	}
 
 	nx, ny := s.guardX+dx, s.guardY+dy
@@ -72,26 +80,33 @@ func (s *simulator) step() (bool, error) {
 		switch to {
 		case '#':
 			// Blocked! Guard turns.
-			fmt.Println("guard rotating")
 			guard, err := rotate(guard)
 			if err != nil {
-				return false, err
+				return false, false, err
 			}
 			s.grid[s.guardY][s.guardX] = guard
 		case '.', 'X':
 			// Clear! Guard walks forward.
+			// Has she been to the new location before?
+			gs := s.guardStates[ny][nx]
+			if slices.Index(gs, guard) >= 0 {
+				// She has!
+				return true, true, nil
+			}
+			// Move her forward and update the places and directions she's been.
 			s.grid[ny][nx] = guard
 			s.grid[s.guardY][s.guardX] = 'X'
+			s.guardStates[s.guardY][s.guardX] = append(s.guardStates[s.guardY][s.guardX], guard)
 			s.guardX, s.guardY = nx, ny
 		default:
-			return false, fmt.Errorf("invalid obstacle: %v", to)
+			return false, false, fmt.Errorf("invalid obstacle: %q", string(to))
 		}
 	} else {
 		// Moving off the grid.
 		s.grid[s.guardY][s.guardX] = 'X'
-		return true, nil
+		return true, false, nil
 	}
-	return false, nil
+	return false, false, nil
 }
 
 // findGuard finds the guard's coordinates in the grid.
@@ -122,29 +137,101 @@ func countX(grid [][]byte) int {
 	return total
 }
 
+// makeGuardStates makes a guardStates grid.
+func makeGuardStates(grid [][]byte) [][][]byte {
+	gs := make([][][]byte, 0, len(grid))
+	for _, row := range grid {
+		gsRow := make([][]byte, 0, len(row))
+		for _, _ = range row {
+			gsRow = append(gsRow, make([]byte, 0))
+		}
+		gs = append(gs, gsRow)
+	}
+	return gs
+}
+
+// copyGrid deep-copies a grid.
+func copyGrid(grid [][]byte) [][]byte {
+	ng := make([][]byte, 0, len(grid))
+	for _, row := range grid {
+		ng = append(ng, slices.Clone(row))
+	}
+	return ng
+}
+
+var errTimeout error = errors.New("timeout")
+
 // simulateGuard simulates the movement of the guard and returns the number of locations she visits.
+// Also returns whether she looped.
 // This mutates the grid.
-func simulateGuard(grid [][]byte) (int, error) {
+func simulateGuard(grid [][]byte) (int, bool, error) {
 	x, y, err := findGuard(grid)
 	if err != nil {
-		return 0, err
+		return 0, false, err
 	}
+	loop := true
 	s := simulator{
-		guardX: x,
-		guardY: y,
-		grid:   grid,
+		guardX:      x,
+		guardY:      y,
+		grid:        grid,
+		guardStates: makeGuardStates(grid),
 	}
-	for {
-		done, err := s.step()
+	// The only way to exceed maxIter is an uncaught loop.
+	maxIter := len(grid) * len(grid[0]) * 4
+	nIter := 0
+	for nIter < maxIter {
+		nIter += 1
+		var done bool
+		done, loop, err = s.step()
 		if err != nil {
-			return 0, err
+			return 0, false, err
 		}
-		if done {
+		if done || loop {
 			break
 		}
 	}
-	printGrid(grid)
-	return countX(grid), nil
+	if maxIter == nIter {
+		return 0, false, errTimeout
+	}
+	return countX(grid), loop, nil
+}
+
+// obstruct places an obstruction at the given location in the grid and returns a new grid.
+func obstruct(grid [][]byte, x, y int) [][]byte {
+	grid = copyGrid(grid)
+	grid[y][x] = '#'
+	return grid
+}
+
+// countLoopObstructions counts how many obstructions can possibly cause the guard to loop.
+func countLoopObstructions(grid [][]byte) (int, error) {
+	// Brute-force solution.
+	bar := progressbar.Default(int64(len(grid) * len(grid[0])))
+	n := 0
+	for y, row := range grid {
+		for x, v := range row {
+			bar.Add(1)
+			switch v {
+			case '.':
+				modifiedGrid := obstruct(grid, x, y)
+				_, loop, err := simulateGuard(modifiedGrid)
+				if err != nil && !errors.Is(err, errTimeout) {
+					return 0, err
+				}
+				if loop || errors.Is(err, errTimeout) {
+					// Sometimes the guard gets stuck in an infinite loop.
+					// This is of course a loop.
+					// I should fix this a different way, but it's easy enough
+					// to detect that it's not worth the time.
+					n++
+				}
+			default:
+				// Already something here.
+				continue
+			}
+		}
+	}
+	return n, nil
 }
 
 func main() {
@@ -155,9 +242,15 @@ func main() {
 	}
 
 	grid := parse(data)
-	n, err := simulateGuard(grid)
+	n, _, err := simulateGuard(copyGrid(grid))
 	if err != nil {
 		fmt.Printf("error simulating guard: %v\n", err)
 	}
 	fmt.Printf("1: %d\n", n)
+
+	n, err = countLoopObstructions(grid)
+	if err != nil {
+		fmt.Printf("error obstructing guard: %v\n", err)
+	}
+	fmt.Printf("2: %d\n", n)
 }
